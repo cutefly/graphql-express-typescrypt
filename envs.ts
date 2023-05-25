a625c6ff9caacb72bb20460ee352fac3f0ab9a3b
Chris Choi <prettyfly@korea.com>
2023-05-25 13:04:41 +0900

    kafka configuration

--- envs.ts
+++ envs.ts
@@ -6,4 +6,8 @@ export default {
     port: process.env.PORT || 3000,
     graphqlPath: process.env.GRAPHQL_PATH || '/graphql',
     dbUrl: process.env.DB_URL || 'mongodb://localhost:27017/graphql-starter',
+    kafka: {
+        broker: process.env.KAFKA_BROKER_URL || 'localhost:9092',
+        clientId: process.env.KAFKA_CLIENT_ID || 'node-app',
+    }
 }
--- kafka/producer.ts
+++ kafka/producer.ts
@@ -2,8 +2,8 @@ import { Kafka, Partitioners  } from 'kafkajs'
 import envs from '../envs'
 
 const kafka = new Kafka({
-    clientId: 'node-app',
-    brokers: ['localhost:9092']
+    clientId: envs.kafka.clientId,
+    brokers: [envs.kafka.broker]
 })
 const producer = kafka.producer({ createPartitioner: Partitioners.LegacyPartitioner })
 producer.connect()
